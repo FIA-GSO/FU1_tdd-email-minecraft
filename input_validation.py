0fbc505d3bb10a3847842e69c248e9a4aed65580
kerem <kerem.s11@gso.schule.koeln>
2023-09-21 14:19:58 +0200

    Valid Password

--- input_validation.py
+++ input_validation.py
@@ -8,7 +8,20 @@ def is_valid_email(email:str) -> bool:
     True -- email is valid 
     False -- email is not valid
     """
-    pattern = r'^[\w!#$%&\'*+\-/=?^_`{|}~]+(\.[\w!#$%&\'*+\-/=?^_`{|}~]+)*' \
-              r'@(((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|' \
-              r'(([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,})))$'
+    pattern = r'^[\w!#$%&\'*+\-/=?^_`{|}~]+(\.[\w!#$%&\'*+\-/=?^_`{|}~]+)*@(((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,})))$'
     return re.match(pattern, email) is not None
+
+
+def is_valid_password(password:str) -> bool:
+
+    mindestlaenge = r'(?=.{8,})'
+    kleinbuchstabe = r'(?=.*[a-z])'
+    grossbuchstabe = r'(?=.*[A-Z])'
+    ziffer = r'(?=.*\d)'
+    sonderzeichen = r'(?=.*[@#$%^&+=!])'
+    keine_leerzeichen = r'[^\s]+'
+
+    regex_muster = f'^{kleinbuchstabe}{grossbuchstabe}{ziffer}{sonderzeichen}{mindestlaenge}{keine_leerzeichen}$'
+    """print(regex_muster)"""
+    return re.match(regex_muster,password) is not None
+    
--- test_input_validation.py
+++ test_input_validation.py
@@ -1,5 +1,5 @@
 import pytest
-from input_validation import is_valid_email
+from input_validation import is_valid_email,is_valid_password
 
 @pytest.mark.parametrize("email", [
     ("test@email.com")
@@ -28,6 +28,7 @@ def test_is_valid_email__gueltige_Adressen(email):
 ,   ("++test@em@ail.com") # Mehrfaches +-Zeichen
 ,   ("ätöüest@gmail.com") # ÄÜÖ 
 ,   ("iopjroiiowcioweoiwednoqoiodoiqwdoiwendoweniwniowendiweoidijsdjascoiwcnweoilajsdjknoicniowesadniowenwe@gmail.com")
+,    
 ])
 def test_is_valid_email__ungueltige_Adressen(email):
     # arrange
@@ -38,3 +39,26 @@ def test_is_valid_email__ungueltige_Adressen(email):
     
     # assert
     assert response is False
+
+@pytest.mark.parametrize("passwords", [
+   ("test1234"),   # Password 8 Zeichen lang
+   ("teSt!234"),      # Passwort mit 8 Zeichen, Sonderzeichen und Großbuchstaben
+   ("nigeroOderS0binKEINj4p4n3r!!!"), # Hakans Passwort 
+   ("ABCdfg!23")
+])
+
+def test_is_valid_password(passwords):
+    response = is_valid_password(passwords)
+    assert response is True
+
+@pytest.mark.parametrize("password", [
+   (r"!$%&/()"), # Passwort nur mit Sonderzeichen
+   ("12345678"), # Passwort nur mit Zahlen
+   ("asdjncrj"), # Passwort nur mit Buchstaben
+   ("ASDFghbj"), # Passwort mit Groß und Klein buchstaben
+   ("")
+])
+
+def test_is_valid_password_is_not_valid(password):
+    responses = is_valid_password(password)
+    assert responses is False
