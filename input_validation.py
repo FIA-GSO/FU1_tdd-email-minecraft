8724f460810fcab0788029c2765eed78abaed2f1
kerem <kerem.s11@gso.schule.koeln>
2023-09-21 14:35:10 +0200

    Anpassung

--- input_validation.py
+++ input_validation.py
@@ -14,14 +14,26 @@ def is_valid_email(email:str) -> bool:
 
 def is_valid_password(password:str) -> bool:
 
-    mindestlaenge = r'(?=.{8,})'
     kleinbuchstabe = r'(?=.*[a-z])'
     grossbuchstabe = r'(?=.*[A-Z])'
     ziffer = r'(?=.*\d)'
     sonderzeichen = r'(?=.*[@#$%^&+=!])'
     keine_leerzeichen = r'[^\s]+'
-
-    regex_muster = f'^{kleinbuchstabe}{grossbuchstabe}{ziffer}{sonderzeichen}{mindestlaenge}{keine_leerzeichen}$'
+    regex_muster = ""
+    
+    if len(password) == 8:
+        mindestlaenge = r'(?=.{8})'
+        kleinbuchstabe = r'(?=.*[a-zA-Z])'
+        regex_muster = f'^{kleinbuchstabe}{ziffer}{sonderzeichen}{mindestlaenge}{keine_leerzeichen}$'
+        
+    if len(password) > 8 and  len(password) <= 20:
+        mindestlaenge = r'(?=.{8,20})'
+        regex_muster = f'^{kleinbuchstabe}{grossbuchstabe}{ziffer}{sonderzeichen}{mindestlaenge}{keine_leerzeichen}$'
+        
+    if len(password) >= 25:
+        mindestlaenge = r'(?=.{25,})'
+        regex_muster = f'^{kleinbuchstabe}{sonderzeichen}{mindestlaenge}{keine_leerzeichen}$'
+        
     """print(regex_muster)"""
     return re.match(regex_muster,password) is not None
     
